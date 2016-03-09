6cff9848a4ce2a4f0d5caf295b21ad03d9f3a662
Aesen Vismea <aesen@unascribed.com>
2016-03-09 02:02:18 -0500

    Correct mistakes and add comments for AffineTransform stuff

--- src/main/java/com/unascribed/blockrenderer/BlockRenderer.java
+++ src/main/java/com/unascribed/blockrenderer/BlockRenderer.java
@@ -259,9 +259,10 @@ public class BlockRenderer {
 			mc.entityRenderer.setupOverlayRendering();
 			RenderHelper.enableGUIStandardItemLighting();
 			/*
-			 * The GUI scale affects us due to the position in the render pipeline
-			 * that our hook is at. As such, we need to counteract this to always
-			 * get a 512x512 render.
+			 * The GUI scale affects us due to the call to setupOverlayRendering
+			 * above. As such, we need to counteract this to always get a 512x512
+			 * render. We could manually switch to orthogonal mode, but it's just
+			 * more convenient to leverage setupOverlayRendering.
 			 */
 			float scale = size/(16f*res.getScaleFactor());
 			GlStateManager.translate(0, 0, -(scale*100));
@@ -329,14 +330,28 @@ public class BlockRenderer {
 	
 	private static BufferedImage createFlipped(BufferedImage image) {
 		AffineTransform at = new AffineTransform();
+		/*
+		 * Creates a compound affine transform, instead of just one, as we need
+		 * to perform two transformations.
+		 * 
+		 * The first one is to scale the image to 100% width, and -100% height.
+		 * (That's *negative* 100%.)
+		 */
 		at.concatenate(AffineTransform.getScaleInstance(1, -1));
+		/**
+		 * We then need to translate the image back up by it's height, as flipping
+		 * it over moves it off the bottom of the canvas.
+		 */
 		at.concatenate(AffineTransform.getTranslateInstance(0, -image.getHeight()));
 		return createTransformed(image, at);
 	}
 	
 	private static BufferedImage createTransformed(BufferedImage image, AffineTransform at) {
+		// Create a copy of the image...
 		BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		// ...get it's renderer...
 		Graphics2D g = newImage.createGraphics();
+		/// ...and draw the old image on top of it with our transform.
 		g.transform(at);
 		g.drawImage(image, 0, 0, null);
 		g.dispose();
