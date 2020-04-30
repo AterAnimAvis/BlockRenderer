7b901e083090b3fd7e2631b112171f247904f542
AterAnimAvis <AterAnimAvis@gmail.com>
2020-04-30 20:08:41 +0100

    Enable keyboard repeating events for EnterNamespaceScreen ---
    - This fixes Backspacing and a couple of other things.
    
    Signed-off-by: AterAnimAvis <AterAnimAvis@gmail.com>

--- src/main/java/com/unascribed/blockrenderer/screens/EnterNamespaceScreen.java
+++ src/main/java/com/unascribed/blockrenderer/screens/EnterNamespaceScreen.java
@@ -26,6 +26,7 @@ public class EnterNamespaceScreen extends BaseScreen {
 	@Override
 	public void init() {
 		assert minecraft != null;
+		minecraft.keyboardListener.enableRepeatEvents(true);
 
 		boolean enabled = minecraft.world != null;
 
@@ -44,6 +45,12 @@ public class EnterNamespaceScreen extends BaseScreen {
 	public void tick() {
 		super.tick();
 		text.tick();
+	}
+
+	@Override
+	public void removed() {
+		assert minecraft != null;
+		minecraft.keyboardListener.enableRepeatEvents(false);
 	}
 
 	@Override
