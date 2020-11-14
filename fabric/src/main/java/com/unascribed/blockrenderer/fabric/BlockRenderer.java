52399461589cb6f6a0f457f5cb97c54be4e99bd0
AterAnimAvis <AterAnimAvis@gmail.com>
2020-11-14 16:15:12 +0000

    refactor: BlockRenderer doesn't need a custom Logger Instance ---
    Signed-off-by: AterAnimAvis <AterAnimAvis@gmail.com>

--- fabric/src/main/java/com/unascribed/blockrenderer/fabric/BlockRenderer.java
+++ fabric/src/main/java/com/unascribed/blockrenderer/fabric/BlockRenderer.java
@@ -2,16 +2,14 @@ package com.unascribed.blockrenderer.fabric;
 
 import com.unascribed.blockrenderer.Reference;
 import com.unascribed.blockrenderer.fabric.client.init.Keybindings;
+import com.unascribed.blockrenderer.varia.logging.Log;
+import com.unascribed.blockrenderer.varia.logging.Markers;
 import net.fabricmc.api.ClientModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class BlockRenderer implements ClientModInitializer {
 
-    public static final Logger LOGGER = LogManager.getLogger(Reference.NAME);
-
     public BlockRenderer() {
-        LOGGER.info("Running Version: " + Reference.VERSION);
+        Log.info(Markers.ROOT, "Running Version: " + Reference.VERSION);
     }
 
     @Override
