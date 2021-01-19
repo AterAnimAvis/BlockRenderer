b45ae4682787df457f526fd42053d4b4a9b19a27
AterAnimAvis <AterAnimAvis@gmail.com>
2021-01-19 18:25:30 +0000

    fix(fabric): Change Mixin to be less intrusive ---
    Signed-off-by: AterAnimAvis <AterAnimAvis@gmail.com>

--- fabric/src/main/java/com/unascribed/blockrenderer/fabric/mixin/MixinMinecraftClient.java
+++ fabric/src/main/java/com/unascribed/blockrenderer/fabric/mixin/MixinMinecraftClient.java
@@ -2,20 +2,16 @@ package com.unascribed.blockrenderer.fabric.mixin;
 
 import com.unascribed.blockrenderer.fabric.client.proxy.ClientProxy;
 import net.minecraft.client.Minecraft;
-import net.minecraft.profiler.IProfiler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraftClient {
 
-    @Redirect(method = "runGameLoop(Z)V", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/profiler/IProfiler;endStartSection(Ljava/lang/String;)V", args = "ldc=gameRenderer"), allow = 1)
-    public void hookGameRenderer(IProfiler profiler, String arg) {
-        profiler.endStartSection(arg);
-
-        //--------------------------------------------------------------------------------------------------------------
-
+    @Inject(method = "runGameLoop(Z)V", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/profiler/IProfiler;endStartSection(Ljava/lang/String;)V", args = "ldc=gameRenderer"), allow = 1)
+    public void hookGameRenderer(CallbackInfo ci) {
         ClientProxy.onFrameStart();
     }
 
